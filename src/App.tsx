4ee9b8beed3293e2f888e6e3c27f1a68b7dfa057
Alex Zambrano <azambri82@gmail.com>
2022-05-10 13:35:05 -0500

    add sign

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { AppRouters } from "./routers/AppRouters";
+import './styles/styleapp.scss'
 
 function App() {
   return (
     <>
       <AppRouters/>
+      <div className="subtitle-section">
+      <h6>Desarrollado por el ingeniero Alex Eduardo Zambrano Perez</h6>
+      <a href="https://www.linkedin.com/in/alex-eduardo-zambrano-perez-aa445915b/">linkedIn</a>
+      </div>
     </>
   );
 }
